d4174368044ef1f3ba29b6f2bc51684409268b0b
satyamsharma <satyamsharma@users.noreply.github.com>
2025-10-19 16:21:09 -0400

    [sharedPtr] more fixes

--- sharedPtr.hpp
+++ sharedPtr.hpp
@@ -1,59 +1,90 @@
-#include <iostream>
-
 namespace stl
 {
     template <typename T>
     class SharedPtr
     {
         public:
-            SharedPtr(T* input) : data{input}, refCount{new int{0}}
+            SharedPtr(T* input) : data{input}, refCount{new int{1}}
             {}
 
-            SharedPtr(SharedPtr& sharedPtr)
+            SharedPtr(const SharedPtr& sharedPtr)
             {
                 data = sharedPtr.data;
-                *sharedPtr.refCount++;
+                (*sharedPtr.refCount)++;
                 refCount = sharedPtr.refCount;
             }
 
-            SharedPtr(SharedPtr&& sharedPtr)
+            SharedPtr(SharedPtr&& sharedPtr) noexcept
             {
                 data = sharedPtr.data;
                 refCount = sharedPtr.refCount;
-
 
                 sharedPtr.data = nullptr;
                 sharedPtr.refCount = nullptr;
             }
 
-            SharedPtr& operator=(SharedPtr& sharedPtr)
+            SharedPtr& operator=(const SharedPtr& sharedPtr)
             {
-                data = sharedPtr.data;
-                *sharedPtr.refCount++;
-                refCount = sharedPtr.refCount;
+                if (this != &sharedPtr)
+                {
+                    if (refCount) (*refCount)--;
+
+                    if (refCount && *refCount == 0)
+                    {
+                        delete refCount;
+                        refCount = nullptr;
+
+                        delete data;
+                        data = nullptr;
+                    }
+
+                    data = sharedPtr.data;
+                    (*sharedPtr.refCount)++;
+                    refCount = sharedPtr.refCount;
+                }
 
                 return *this;
             }
 
-            SharedPtr& operator=(SharedPtr&& sharedPtr)
+            SharedPtr& operator=(SharedPtr&& sharedPtr) noexcept
             {
-                data = sharedPtr.data;
-                refCount = sharedPtr.refCount;
+                if (this != &sharedPtr)
+                {
+                    if (refCount) (*refCount)--;
 
+                    if (refCount && *refCount == 0)
+                    {
+                        delete refCount;
+                        refCount = nullptr;
 
-                sharedPtr.data = nullptr;
-                sharedPtr.refCount = nullptr;
+                        delete data;
+                        data = nullptr;
+                    }
+                 
+                    data = sharedPtr.data;
+                    refCount = sharedPtr.refCount;
+
+                    sharedPtr.data = nullptr;
+                    sharedPtr.refCount = nullptr;
+                }
 
                 return *this;
             }
 
             ~SharedPtr()
             {
-                delete refCount;
-                delete data;
-                refCount = nullptr;
-                data = nullptr;
+                if (refCount) (*refCount)--;
+                
+                if (refCount && *refCount == 0)
+                {
+                    delete refCount;
+                    delete data;
+
+                    refCount = nullptr;
+                    data = nullptr;
+                }
             }
+
         private:
             int* refCount{nullptr};
             T* data{nullptr};
